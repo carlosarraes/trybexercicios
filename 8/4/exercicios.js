702937cc841da533b81228ccc7ab4ac0f53de26a
carraes <carraeshb@gmail.com>
2022-09-26 11:09:25 -0300

    Dia 4

---
+++ 8/4/exercicios.js
@@ -0,0 +1,107 @@
+const arrays = [['1', '2', '3'], [true], [4, 5, 6]];
+
+function flatten(arr) {
+  // escreva seu código aqui
+}
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+function reduceNames(obj) {
+  // escreva seu código aqui
+}
+
+function averageAge(obj) {
+  // escreva seu código aqui
+}
+
+function longestNamedBook(obj) {
+  // escreva seu código aqui
+}
+
+const names = [
+  'Aanemarie',
+  'Adervandes',
+  'Akifusa',
+  'Abegildo',
+  'Adicellia',
+  'Aladonata',
+  'Abeladerco',
+  'Adieidy',
+  'Alarucha',
+];
+
+function containsA(matrix) {
+  // escreva seu código aqui
+}
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
+
+function studentAverage(stnd, grds) {
+  // escreva seu código aqui
+}
---
+++ 8/4/readme.md
@@ -0,0 +1,11 @@
+- [ ] Utilize o reduce para transformar uma matriz em um array.
+- [ ] Crie uma string com os nomes de todas as pessoas autoras.
+      `"George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";`
+- [ ] Calcule a média de idade que as pessoas autoras tinham quando seus respectivos livros foram lançados.
+      `43`
+- [ ] Encontre o livro com o maior nome.
+      `{id: 1, name: 'As Crônicas de Gelo e Fogo', genre: 'Fantasia', author: { name: 'George R. R. Martin', birthYear: 1948,}, releaseYear: 1991,};`
+- [ ] Dado o array de nomes, utilize o reduce para retornar a quantidade de vezes em que aparece a letra a, maiúscula ou minúscula.
+      `20`
+- [ ] Para o próximo exercício você deve criar um novo array de objetos a partir das informações de students e grades, onde cada objeto desse novo array terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição 0 de grades refere-se ao estudante na posição 0 de students. Para resolver este exercício, além de reduce, será necessário utilizar também a função map.
+      ` { name: 'Pedro Henrique', average: 7.8 }, { name: 'Miguel', average: 9.2 }, { name: 'Maria Clara', average: 8.8 },`
